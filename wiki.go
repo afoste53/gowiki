985a87efc4dca2dccf587955bc1ca696c224058f
Gabe <afoste53@gmail.com>
2021-09-28 12:55:11 -0500

    add wrapper fn to validate title

--- wiki.go
+++ wiki.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"regexp"
-	"errors"
 )
 
 type Page struct {
@@ -30,30 +29,8 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-// reg-ex to validate url path
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-z0-9]+)$")
 
-func getTitle(w http.ResponseWriter, r *http.Request)(string, error){
-	m := validPath.FindStringSubmatch(r.URL.Path)
-
-	// if it doesn't match, return 404
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-
-	// the title is stored at m[2]
-	return m[2], nil
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request){
-	// check if page exists and fetch
-	title, err := getTitle(w, r)
-
-	if err != nil {
-		return 
-	}
-
+func viewHandler(w http.ResponseWriter, r *http.Request, title string){
 	p, err := loadPage(title)
 
 	// redirect if page doesn't exist
@@ -65,15 +42,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request){
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request){
-	// check if page exists and fetch it if so
-	title, err := getTitle(w, r)
-
-	if err != nil {
-		return
-	}
-
-
+func editHandler(w http.ResponseWriter, r *http.Request, title string){
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -82,14 +51,7 @@ func editHandler(w http.ResponseWriter, r *http.Request){
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request){
-	// check if page exists and fetch it if so
-	title, err := getTitle(w, r)
-
-	if err != nil {
-		return
-	}
-
+func saveHandler(w http.ResponseWriter, r *http.Request, title string){
 	body := r.FormValue("body")
 
 	p := &Page{Title: title, Body: []byte(body)}
@@ -108,7 +70,7 @@ var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 // fn to render to a given html template
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
-	t, err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 
 	// handle err if not nil
 	if err != nil {
@@ -117,9 +79,29 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 	}
 }
 
+
+// reg-ex to validate url path
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		// get title and call passed in handler fn
+		m := validPath.FindStringSubmatch(r.URL.Path)
+
+		// if title doesn't match, 404
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, m[2])
+	}
+}
+
+
 func main(){
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
